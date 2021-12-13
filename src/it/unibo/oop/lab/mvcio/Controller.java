462a432d7303baa643f7b7c28527ab62e172c274
Giacomo Amadio <giacomo.amadio@studio.unibo.it>
2021-12-13 12:35:37 +0100

    implemented controller class to write on file

--- src/it/unibo/oop/lab/mvcio/Controller.java
+++ src/it/unibo/oop/lab/mvcio/Controller.java
@@ -1,9 +1,46 @@
 package it.unibo.oop.lab.mvcio;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
 /**
  * 
  */
 public class Controller {
+    private static final String HOME = System.getProperty("user.home");
+    private static final String SEPARATOR = System.getProperty("file.separator");
+    private static final String DEFAULT_FILE = "output.txt";
+    private File file = new File(HOME + SEPARATOR + DEFAULT_FILE);
+    /**
+     * @param file
+     *            file to be set as current 
+     */
+    public void setFile(final File file) {
+        this.file = file;
+    }
+    /**
+     * @return the current file
+     */
+    public File getFile() {
+        return this.file;
+    }
+    /**
+     * @return the path of the current file
+     */
+    public String getCurrentPath() {
+        return this.getFile().getPath();
+    }
+    /**
+     * @param message 
+     *              string to be printed
+     * @throws IOException 
+     */
+    public void save(final String message) throws IOException {
+        try (PrintStream ps = new PrintStream(this.file)) {
+            ps.println(message);
+        }
+    }
 
     /*
      * This class must implement a simple controller responsible of I/O access. It
